4cacc567054522df80fc573b46627c730949aa28
hammadurrehman01 <hammadurrehman@techlabs.cc>
2025-04-07 13:13:57 +0500

    refactor(cors): add additional allowed origins for CORS configuration

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ app.use(express.urlencoded({ extended: true }));
 
 const allowedOrigins = [
   "http://localhost:5173",
+  "http://localhost:5174",
   "https://azzipizza-customer.vercel.app",
+  "https://azzi-pizza-admin-panel.vercel.app/",
 ];
 
 const corsOptions = {

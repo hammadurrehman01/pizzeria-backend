26e68ae2fd7a74900845f27dfc7ba51d0ede6ed5
hammadurrehman01 <hammadurrehman@techlabs.cc>
2025-04-08 22:19:32 +0500

    refactor(cors): enhance CORS options for better origin handling

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const allowedOrigins = [
   "http://localhost:5174",
   "https://azzipizza-customer.vercel.app",
   "https://azzi-pizza-admin-panel.vercel.app",
-  "https://www.azzipizza.it/",
+  "https://www.azzipizza.it",
 ];
 
 const corsOptions = {
